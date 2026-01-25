5af63cf085487df1d200d0b0917c6d7bef829be4
Vadim <60077444+VadBull@users.noreply.github.com>
2026-01-25 22:27:32 +0500

    Refine local boundaries plugin layout

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,9 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import layerBoundaries from './eslint-plugin-boundaries/index.js'
+
+const layers = ['app', 'pages', 'features', 'entities', 'shared']
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -22,8 +25,24 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    plugins: {
+      boundaries: layerBoundaries,
+    },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'boundaries/layer-imports': [
+        'error',
+        {
+          layers,
+          aliases: {
+            '@app': 'app',
+            '@pages': 'pages',
+            '@features': 'features',
+            '@entities': 'entities',
+            '@shared': 'shared',
+          },
+        },
+      ],
     },
   },
 ])
